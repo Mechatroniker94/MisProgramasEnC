ad5a8748afaaed91b67135c0956fe25d4f411e44
daniel <danny_elito94@outlook.com>
2020-04-24 12:43:22 -0500

    Programa 6.23 terminado

--- Programa6_23.c
+++ Programa6_23.c
@@ -13,7 +13,7 @@ void avanzar(int b[][TY],int desp,int giro,int estPlum);
 
 int tablero[TX][TY] = {{0},{0}};
 char comandos[6] = {'d','u','r','l','m','x'};
-int estadoPluma =0;
+int estadoPluma = 1;
 int posX = 0;
 int posY = 0;
 
@@ -23,7 +23,7 @@ int main(){
 	int distancia = 0;
 	printf("\tBienvenido a Graficos de tortuga\n");
 	printf("Por favor seleccione una de las siguientes opciones\n");
-	printf("1.- Subir la pluma\n2.- Bajar la pluma\n3.- Girar a la derecha\n4.- Girar a la izquierda\n5.- Mover X adelante\n6.- Visualizar tablero\n7.- Salir\n");
+	printf("1.- Subir la pluma\n2.- Bajar la pluma\n3.- Girar a la derecha\n4.- Girar a la izquierda\n5.- Mover X adelante\n6.- Visualizar tablero\n7.- Estado actual\n8.- Salir\n");
 	scanf("%i",&selector);
 
 	while( selector != 8){
@@ -68,7 +68,7 @@ int main(){
 				printf("La pluma %i\n",estadoPluma);
 				printf("La direccion es %i\n",giro);
 				printf("La posicion actual es %i , %i\n",posX,posY);
-				printf("El valor de la posicion %i,%i es:  %i \n",posX,posY,tablero[posX][posY]);
+				printf("El valor de la posicion %i,%i es:  %i \n",posX,posY,tablero[posX-1][posY]);
 				system("sleep 3");
 
 			case 8:
@@ -81,7 +81,7 @@ int main(){
 		}//Fin del switch
 		system("clear");
 		printf("Por favor seleccione una de las siguientes opciones\n");
-		printf("1.- Subir la pluma\n2.- Bajar la pluma\n3.- Girar a la derecha\n4.- Girar a la izquierda\n5.- Mover X adelante\n6.- Visualizar tablero\n7.- Salir \n");
+		printf("1.- Subir la pluma\n2.- Bajar la pluma\n3.- Girar a la derecha\n4.- Girar a la izquierda\n5.- Mover X adelante\n6.- Visualizar tablero\n7.- Estado actual\n8.- Salir \n");
 		scanf("%i",&selector);
 	}//Fin del while
 
@@ -91,9 +91,13 @@ int main(){
 void mostrarTablero(int b[][TY]){
 	for(int i = 0; i < TX;i++){
 		for(int j = 0; j < TY;j++){
+            //printf("%i ",b[i][j]);
 			if(b[i][j] == 1){
-				printf("* ");
+				printf("1 ");
 			}//Fin del if
+			else{
+			printf("0 ");
+			}
 		}//Fin del for J
 		printf("\n");
 	}//Fin del for I
@@ -106,38 +110,37 @@ void avanzar(int b[][TY],int desp,int giro,int estPlum){
 			for(int i = 0; i < desp; i++){
 				if(estPlum == 1){
 					b[posY][posX] = 1;
-					posY--;
 				}//Fin del if
+				posY--;
 			}//Fin del for
 			break;
 		case 1:			//X aumenta
 			for(int i = 0; i < desp; i++){
 				if(estPlum == 1){
 					b[posY][posX] = 1;
-					posX++;
-				}//Fin del if
+                }//Fin del if
+                posX++;
 			}//Fin del for
 			break;
 		case 2:			//Y aumenta
 			for(int i = 0; i < desp;i++){
 				if(estPlum == 1){
 					b[posY][posX] = 1;
-					posY++;
 				}//Fin del if
+				posY++;
 			}//Fin del for
 			break;
 		case 3:			//X disminuye
 			for(int i = 0; i < desp; i++){
 				if(estPlum == 1){
 					b[posY][posX] = 1;
-					posX--;
 				}//Fin del if
+				posX--;
 			}//Fin del for
 			break;
 		default:
 			break;
 
 	}//Fin del switch
-
 }//Fin de la funcion avanzar
 
